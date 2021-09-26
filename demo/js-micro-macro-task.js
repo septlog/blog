7936cda55c1b6c167328bcb3b3ef3818d8cfa441
septlog <septlog@qq.com>
2021-09-26 17:42:08 +0800

    micro-macro

---
+++ demo/js-micro-macro-task.js
@@ -0,0 +1,15 @@
+new Promise((resolve) => {
+  console.log('外层宏事件2');
+  resolve();
+})
+  .then(() => {
+    console.log('微事件1');
+  })
+  .then(() => {
+    console.log('微事件2');
+  });
+setTimeout(() => {
+  //执行后 回调一个宏事件
+  console.log('内层宏事件3');
+}, 0);
+console.log('外层宏事件1');
---
+++ source/_posts/js-micro-macro-task.md
@@ -0,0 +1,9 @@
+---
+title: js-micro-macro-task
+date: 2021-09-26 17:04:18
+tags:
+---
+
+> queueMicroTask(func) 可以让 func 在微任务中执行
+
+每个宏任务执行后，js 引擎会执行微任务队列中的所有微任务
